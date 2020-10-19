ce85063aadefe8dbc8ed5e7ffba290106d6a6541
jstawik <jakub.stawik@gmail.com>
2020-10-19 22:13:15 +0200

    Remove comments

--- src/main/scala/dev/stawik/wust/dna/Simulator.scala
+++ src/main/scala/dev/stawik/wust/dna/Simulator.scala
@@ -29,13 +29,11 @@ object Simulator extends App {
     val nodeFactory = config.nodeType match {
       case "JoinersLeavers" => joinersLeaversFactory(config.nodeParams.asInstanceOf[JoinersLeaversParams])
       case "ApproxHistograms" => approxHistogramsFactory(config.nodeParams.asInstanceOf[ApproxHistogramsParams])
-      //case _ => println(s"${config.nodeType} not recognized as nodeType")
     }
     val results = (0 to config.iterations).par.map { _ =>
       var reports = Seq.empty[Map[String, Double]]
       val network = config.networkShape match {
         case "Grid" =>  new Grid(config.networkParams.asInstanceOf[GridParams], nodeFactory)
-        //case _ => println(s"${config.networkShape} not recognized as networkShape")
       }
       network.autoStep()
       for(_ <- 0 until config.steps){
@@ -48,45 +46,4 @@ object Simulator extends App {
     println(s" Finished after: ${(System.nanoTime - startTime)/1e9d}")
     ResultWriter.saveJSON(config.toString, results)
   }
-
-//  //case class SimConfig(intervals: Int, variables: Int, gridSideA: Int, gridSideB: Int, iterations: Int, steps: Int)
-//  //val config = fromFile("config.json").mkString.asJsonObject(EncoderOps[Seq[SimConfig]])
-//  val intervals = 20
-//  val variables = 50
-//  val gridSide = 5
-//  val iterations = 32 //14min
-//  val steps = 6000
-//
-//  println("Starting")
-//
-//  //val singleRun = Future[Seq[Map[String, Double]]] {
-//  //  var reports = Seq.empty[Map[String, Double]]
-//  //  val networkNH = new Network[NodeNH](Grid(gridSide, gridSide), () => new NodeNH(intervals, variables))
-//  //  networkNH.autoStep()
-//  //  reports = reports appended networkNH.report()
-//  //  for (_ <- 0 until steps) {
-//  //    networkNH.updateValues(.1 / intervals)
-//  //    networkNH.autoStep()
-//  //    reports = reports appended networkNH.report()
-//  //  }
-//  //  reports
-//  //}
-//
-//
-//  val resultsNH: ParSeq[Seq[Map[String, Double]]] = (0 until iterations).par.map(_ => {
-//  var reports = Seq.empty[Map[String, Double]]
-//  val networkNH = new Network[JoinersLeavers](Grid(gridSide, gridSide), () => new JoinersLeavers(intervals, variables))
-//  networkNH.autoStep()
-//  reports = reports appended networkNH.report()
-//  for (_ <- 0 until steps) {
-//    networkNH.updateValues(.5 / intervals)
-//    networkNH.autoStep()
-//    reports = reports appended networkNH.report()
-//  }
-//  reports
-//}
-//)
-//
-//  println("Done")
-//
 }
