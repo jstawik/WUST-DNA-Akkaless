86cd745eb40c4f34cc2fc5a4240e1cae6d706d6a
jstawik <jakub.stawik@gmail.com>
2021-01-12 01:13:07 +0100

    Add progress tracking
    Yes my PC was running for 20th hour in a row. At 100% CPU.
    I killed the job - but maybe it was about to finish?

--- src/main/scala/dev/stawik/wust/dna/Simulator.scala
+++ src/main/scala/dev/stawik/wust/dna/Simulator.scala
@@ -10,7 +10,7 @@ import dev.stawik.wust.dna.network.GridClique.{gridQFactory, GridQParams}
 import dev.stawik.wust.dna.network.PBCGrid.pbcGridFactory
 
 import scala.collection.parallel.CollectionConverters.ImmutableIterableIsParallelizable
-
+import scala.collection.mutable.BitSet
 object Simulator extends App {
   var configPath: String = "."
   if (args.length != 0 ){
@@ -25,7 +25,7 @@ object Simulator extends App {
     .map(new ConfigReader(_)).map(_.config).collect{case Right(config) => config}.map(executeConfig)
 
   def executeConfig(config: Config): Unit = {
-    print(s"Executing job: $config.")
+    println(s"Executing job: $config.")
     val startTime = System.nanoTime
     val nodeFactory = config.nodeType match {
       case "JoinersLeavers" => joinersLeaversFactory(config.nodeParams.asInstanceOf[JoinersLeaversParams])
@@ -36,15 +36,21 @@ object Simulator extends App {
       case "GridClique" => gridQFactory(config.networkParams.asInstanceOf[GridQParams], nodeFactory)
       case "PBCGrid" => pbcGridFactory(config.networkParams.asInstanceOf[GridParams], nodeFactory)
     }
-    val results = (0 to config.iterations).par.map { _ =>
+
+    val itersDone = BitSet.empty
+    val lineRemove = "\u001B[A\r\u001B[2K"
+    println(s"Iterations done: 0/${config.iterations}")
+    val results = (0 to config.iterations).par.map { iter =>
       var reports = Seq.empty[Map[String, Double]]
       val network = networkFactory()
-      network.autoStep()
+      //network.autoStep()
       for(_ <- 0 until config.steps){
         network.updateValues(config.scale)
         network.autoStep()
         reports = reports appended network.report()
       }
+      itersDone += iter
+      println(lineRemove + s"Iterations done: ${itersDone.size}/${config.iterations}, time elapsed: ${(System.nanoTime - startTime)/1e9d}")
       reports
     }.toSeq
     println(s" Finished after: ${(System.nanoTime - startTime)/1e9d}")
